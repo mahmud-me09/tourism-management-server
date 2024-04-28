5ca7bf3f795a9a8875b34137e97b4db59e269871
mahmud-me09 <mahmud.me09@gmail.com>
2024-04-28 20:49:29 +0600

    specific id query

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-live server: https://tourism-management-server-bbnjm22yg-mahmud-hasan-khans-projects.vercel.app/
+live server: https://tourism-management-server-nine.vercel.app/
+
+production link: https://tourism-management-server-evgholbwf-mahmud-hasan-khans-projects.vercel.app
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cors());
+app.use((req, res, next) => {
+	res.header({ "Access-Control-Allow-Origin": "*" });
+	next();
+}); 
 
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.PASSWORD}@cluster0.r7w9ywx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,10 +27,46 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		// await client.connect();
+		const touristSpotCollection = client.db("touristSpotDB").collection("touristSpot");
+		
+		const countryCollection = client.db("countryDB").collection("country");
 
 		// Express setup
 		app.get("/", (req, res) => {
 			res.send("Hello World!");
+		});
+		app.get("/country", async(req,res)=>{
+			try {
+				const cursor = countryCollection.find();
+				const result = await cursor.toArray();
+				res.send(result);
+			} catch (error) {
+				console.error(
+					"Error fetching data from country collection:",
+					error
+				);
+				res.status(500).send(
+					"Error fetching data from country collection"
+				);
+			}
+		})
+		app.get("/alltouristsspot/:id",async(req,res)=>{
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const touristSpot = await touristSpotCollection.findOne(query);
+			res.send(touristSpot);
+		})
+
+		app.get("/alltouristsspot", async(req,res)=>{
+			const cursor = touristSpotCollection.find()
+			const result = await cursor.toArray()
+			res.send(result)
+		});
+
+		app.post("/addtouristsspot", async (req, res) => {
+			const newTouristSpot = req.body;
+			const result = await touristSpotCollection.insertOne(newTouristSpot)
+			res.send(result)
 		});
 
 		app.listen(port, () => {
@@ -38,7 +80,7 @@ async function run() {
 		);
 	} finally {
 		// Ensures that the client will close when you finish/error
-		await client.close();
+		// await client.close();
 	}
 }
 run().catch(console.dir);
